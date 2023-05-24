2d198fd40cfe5abb79295633aa06177c700af9c8
Dima Kruglov <dkruglov@unislabs.com>
2023-05-25 01:31:13 +0300

    error graph

--- models.py
+++ models.py
@@ -1,21 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([0, 50, 100, 200, 400, 800, 1600, 3200])
+x = np.array([10, 50, 100, 200, 400, 600, 800, 1000, 1400, 1800, 2500, 5000])
 
-y1 = np.array([0, 0.115, 0.226, 0.465, 0.943, 1.843, 3.685, 7.41])
+y1 = np.array([4, 8, 5, 2, 4, 5, 3, 2, 1.5, 1.1, 1.6, 1.5])
 
-y2 = np.array([0, 0.122, 0.249, 0.522, 1.078, 2.137, 4.22, 8.58])
 
-y3 = np.array([0, 0.122, 0.236, 0.485, 0.981, 1.98, 3.91, 7.692])
 
-lines = plt.plot(x, y1, x, y2, x, y3)
+lines = plt.plot(x, y1,)
 
 plt.xlabel("Количество меток")
 
-plt.ylabel("Время оценки, с")
-
-plt.legend(['Pefsa', 'Adaptive Q', 'Subep-q'])
+plt.ylabel("Ошибка оценки, %")
 
 plt.grid()
 
